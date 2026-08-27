236754a33b06c2187af05f3148fc171052d6faf7
PedroMPaula <pedromp.infological2006@gmail.com>
2026-08-27 01:43:42 -0300

    chore: inicializa projeto MiniMES

---
+++ src/main/java/br/com/minimes/minimes/MinimesApplication.java
@@ -0,0 +1,13 @@
+package br.com.minimes.minimes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinimesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinimesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/minimes/minimes/MinimesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.minimes.minimes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinimesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
